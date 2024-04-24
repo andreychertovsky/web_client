f5c7afe5b30099f6ccaf56945ace7d83a181f5b9
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2024-04-24 13:22:47 +0800

    feat: add Dialog component

---
+++ src/shared/ui/dialog/script.ts
@@ -0,0 +1,66 @@
+import { closeDialogEvent, openDialogEvent } from "@/shared/lib/event-bus";
+
+class Dialog extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const dialogElement = this.querySelector("dialog")!;
+    const bodyElement = document.body;
+
+    openDialogEvent.subscribe(({ name }) => {
+      if (this.dataset.name === name) {
+        dialogElement.showModal();
+        bodyElement.style.overflow = "hidden";
+      }
+    });
+
+    closeDialogEvent.subscribe(({ name }) => {
+      if (this.dataset.name === name) {
+        dialogElement.close();
+        bodyElement.removeAttribute("style");
+      }
+    });
+  }
+}
+
+customElements.define("wc-dialog", Dialog);
+
+class DialogOpenTrigger extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const triggerElement = this.children[0]!;
+    const name = this.dataset.name ?? "";
+    const handleTriggerClick = (event: Event) => {
+      event.preventDefault();
+      openDialogEvent.broadcast({ name });
+    };
+
+    triggerElement.addEventListener("click", handleTriggerClick);
+  }
+}
+
+customElements.define("wc-dialog-open-trigger", DialogOpenTrigger);
+
+class DialogCloseTrigger extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const triggerElement = this.children[0]!;
+    const name = this.dataset.name ?? "";
+    const handleTriggerClick = (event: Event) => {
+      event.preventDefault();
+      closeDialogEvent.broadcast({ name });
+    };
+
+    triggerElement.addEventListener("click", handleTriggerClick);
+  }
+}
+
+customElements.define("wc-dialog-close-trigger", DialogCloseTrigger);
