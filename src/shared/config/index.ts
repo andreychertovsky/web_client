da77ede11ed2b76184969699a68b314ebfe4f215
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2024-06-07 17:30:08 +0300

    feat: add link to support email

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -4,3 +4,4 @@ export const LINK_TO_CHROME_EXTENSION =
   "https://chromewebstore.google.com/detail/product-search/inbbmabopknohmlmilkhjdidlmbhhofd";
 export const LINK_TO_APPLE_STORE = "https://apps.apple.com/";
 export const LINK_TO_GOOGLE_PLAY = "https://play.google.com/";
+export const EMAIL_SUPPORT = "support@productsearch.app";
